218a1073e0893ddde2d66f3c4b9fdf5d64b1bcca
ASobirjonov074 <jonic607@gmail.com>
2023-07-19 18:14:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// let names = ["Alex","George","Michael"]
+// function name(names) {
+// return names.reduce((a,b) => (b.length > a.length) ? b : a)
+// }
+// let theLongestName = name()
+// alert(theLongestName)
+
+
+// function math() {
+//     let a = +prompt()
+//     let b = +prompt()
+//     alert(`
+//     ${a} + ${b} = ${a + b} 
+//     ${a} / ${b} = ${a / b} 
+//     ${a} - ${b} = ${a - b} 
+//     ${a} * ${b} = ${a * b} 
+//     ${a}  ${b} = ${a  b} 
+//     ${a} % ${b} = ${a % b} 
+//     `)
+// }
+
+
+
+
+
